cea129ed686f2ab7210a2e7161a07669f12e74f2
Antigoni Bitou <antibit1991@gmail.com>
2018-07-01 00:20:20 +0300

    Work on main menu

--- K.O.K/Assets/Scripts/MainMenu.cs
+++ K.O.K/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour {
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exiting application");
+        Application.Quit();
+    }
 }
